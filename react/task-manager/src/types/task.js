473514f725288c4903233b086a19b6459c8ba5bf
Priyak Dey <priyak1991dey@gmail.com>
2024-05-02 23:45:33 +0530

    remove task-manager project

--- react/task-manager/src/types/task.js
+++
@@ -1,9 +0,0 @@
-export class Task {
-    constructor(id, title, status, dueDate, priority) {
-        this.id = id;
-        this.title = title;
-        this.status = status;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-}
